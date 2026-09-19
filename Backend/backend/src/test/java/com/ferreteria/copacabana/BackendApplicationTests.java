e6a30a5139ec6920966b66f5a83cb9ec503bff2f
ALVARO OCIEL SANCHEZ MAGNE <lpze.alvaroociel.sanchez.ma@unifranz.edu.bo>
2026-09-19 12:02:21 -0400

    Configuration initial

---
+++ Backend/backend/src/test/java/com/ferreteria/copacabana/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferreteria.copacabana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
